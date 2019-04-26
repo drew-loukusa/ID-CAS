4b7ca713fb352e5bf3564d998902daaef7faf473
drew-loukusa <dlwerd@gmail.com>
2019-04-25 17:13:11 -0700

    Changed how the lexing works slightly

--- lexer.py
+++ lexer.py
@@ -1,69 +1,118 @@
+VAR = "x"
+EOS = "$"
+NUMS = "012345789"
+OPS = "+*/-^"
+TRIG = "sct"
 
 class Lexer:
 
-	def __init__(self):
-		self.x = 0
-	
+	def __init__(self):		
+		pass
+		
 	def Lex(self, text):
-
-		NUMS = "012345789"
-		VAR = "x"
-		OPS = "+*/-^"
-		EOS = "$"
-
+	
 		#Add EOS char to string:
 		text = text + "$"
+	
+		# Insert explicit multiplication symbols: 
+		text = self._insert_explicit_mult(text)
+		#print(text)
 
+		char = ""
 		last = None	
-		build_num = False
-		cur_num = ""
-
-
+		cur_num = ""		
 		explicit_text = []
 
-		# =============== Transform Text ============== #		
-		for char in text:
+		# ==================== Split Text into Tokens: ====================== #
+		i = 0
+		token = ""
+		while i < len(text):
+			char = text[i]			
+			#print(last,char, i)		
+
 			# ------------- Handle Multi Digit Numbers ------------- #
 			if char in NUMS:
-				print(last,char, build_num)
+				token = self._parse_num(text, i)		
+				#print("\t",i, token)
+				i += len(token) - 1
+				#print("\t",i)				
+				char = text[i]
 				
-				# If the last char was a num -> Keep building num:				
-				if last and (last in NUMS and char in NUMS):
-					cur_num += char
-					
-				# If last NOT a num -> Start building the number:
-				if cur_num == "": 
-					cur_num = char
-					build_num = True					
-
-			# If we were building a multi-digit number and it ends,
-			# Then add it to the list of tokens and reset the vars
-			if (char in OPS or char in VAR or char in EOS) and build_num:
-				build_num = False
-				explicit_text.append(cur_num)	
-				cur_num = ""
+			# ------------- Handle Trig Functions ------------------ #
+			elif char in TRIG:
+				token = self._parse_trig(text, i)		
+				i += len(token) - 1
+				#print("\t",i)				
+				char = text[i]
 				
+			# Add any single char as necessary:
+			elif char in VAR + OPS: 
+				token = char		
+			#print(last,char)		
+			
+			if char != EOS: explicit_text.append(token)
+			
+			last = char			
+			i += 1
 
-			# -------- Insert explicit multiplication symbols: --------- #
+		return explicit_text
 
+	def _insert_explicit_mult(self, text):
+		i = 0
+		char = text[i]
+		last = None
+		text = list(text)
+		while char != "$":					
 			# If we have a VAR and a NUM next to each other, insert a mult op:
 			# 	This handles left and right associativity: "x5" and "5x"
 			if last and ((char in NUMS and last in VAR) or
 						 (char in VAR and last in NUMS)):			
-				explicit_text.append("*")
+				text.insert(i, "*")				
+			i += 1
+			last = char
+			char = text[i]
+		textstr = ""
+		for char in text: textstr+=char
+		return textstr
 
-			# Set cur char to last char to allow for multi-char tokens:
-			last = char			
+	def _parse_num(self, text, index):
+		'''Parses multi-digit numbers and returns them'''		
+		cur_num = ""
+		i = index
+		char = text[i]
+		
+		while char in NUMS:
+			cur_num += char		
+			i += 1		
+			char = text[i]
+			build_num = True
+		
+		return cur_num
+				
+	def _parse_trig(self, text, index):
+		'''Parses trig functions returns a string'''		
+		cur_trig = ""
+		i = index
+		char = text[i]
+		
+		while char in NUMS:
+			cur_num += char		
+			i += 1		
+			char = text[i]
+			build_num = True
+		
+		return cur_num
 
-			# If we're building a number, then we don't
-			# want to add it to the token list yet:
-			if not build_num and char != EOS: 
-				explicit_text.append(char)
-
-		return explicit_text
-
+def test(result, expect):	
+	try:
+		assert result == expect		
+	except AssertionError as e:
+		print("Test failed...")
+		print("  Expected:\t" + str(expect))
+		print("  Received:\t"+str(result))
 	
 
 if __name__ == "__main__":
 	foo = Lexer()
-	print(foo.Lex("x20*300"))
+	test(foo.Lex("20x*300"),	  ['20','*','x','*','300'])
+	test(foo.Lex("20x*300+45-2"), ['20','*','x','*','300','+','45','-','2'])

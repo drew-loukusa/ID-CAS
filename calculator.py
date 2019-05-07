010c3a08e18a2ce840af990a2283a9a488acd86c
drew-loukusa <dlwerd@gmail.com>
2019-05-07 11:14:33 -0700

    Moved the 'Calculator' which combines each piece into it's own module. Renamed 'build_tree_messy' to 'tree' and 'calculator' to 'derivative'.

--- calculator.py
+++ calculator.py
@@ -8,300 +8,114 @@
 #																			   #
 #==============================================================================#
 
-
-
-# TODO: Implement trig derivatives
-
-
-
-from build_tree_messy import NodeType, Copy, Node, DumpTree, PrintTree, check_node_type
+from tree import NodeType, Copy, Node, DumpTree, PrintTree, check_node_type
 check = check_node_type
 
 from sys import stdout as std
 
-def diff(root):	
+def main(args):	
+	from colorama import Fore, Back, Style 	
+	if len(args) < 2:
+		print("No input expression")
+		print(	"  Usage:\n"+
+				"     python3 build_tree_messy.py [expression]" )
+		return 0
+	#================================== Header ===============================#
+	print(Fore.BLUE+"┌"+78*"-"+"┐")	
+	std.write("|"+Fore.RED+"Derivative Calculator".center(78, " ")+Fore.BLUE+"|\n")	
+	print("└"+78*"-"+"┘")
+	print(Style.RESET_ALL) 		
+		
+	#====================== Input Info + Preprocessing =======================#
+		
+	input_string = args[1]
 	
-	if is_leaf(root):
-
-		if str(root._Class) == str(NodeType.Identifier): 
-			return Node( NodeType.Literal, str(1), 1 , None , None )
-
-		elif str(root._Class) == str(NodeType.Literal):					
-			return Node( NodeType.Literal, str(0), 0 , None , None )
-
-	elif root._Symbol == "^":		
-		if str(root._Left._Class) == str(NodeType.Identifier) and \
-		   str(root._Right._Class) == str(NodeType.Literal):			
-			
-			""" Case for handling integer exponents: ( expr ) ^ n """
-			
-			#print("Dived an exponent")
-			#print(root)
-			#print(root._Right)
-			#print(root._Left)
-			
-			# Copy the left sub-tree of root:
-			copy = Copy(root._Left)
-		
-		
-def simplify( root,  direction=None, parent=None, debug=False): 
-
-	if root:
-		#if tree_modified: simplify(root._Left, direction, root, debug)
-		if not is_leaf(root._Left): simplify(root._Left,  "left", root, debug)	
-		if not is_leaf(root._Right):simplify(root._Right, "right",root, debug)
-
-	tree_modified = False
-
-	# Debug prints:	
-	if debug and root and root._Symbol == "*":
-		print(root)
-		print("\t"+str(root._Left))
-		print("\t"+str(root._Right))
-
-	#--------------------------- Simplifies x^1 -------------------------------#
-	# If we have a multiplication and ONLY one of the children are leaf nodes:
-	if 	root and root._Symbol == "^" and is_leaf(root._Right):
-		if root._Right._LitVal == 0:
-			int_node = Node( NodeType.Literal, "1", 1, None, None)
-			set_child(parent, root, direction, int_node)
-			
-
-		if root._Right._LitVal == 1:
-			set_child(parent, root, direction, root._Left)
-			
-			
-			
-	# --------------------- Simplifies x*1 and x*0 ----------------------------#
-	# If we have a multiplication and ONLY one of the children are leaf nodes:
-	if 	root and root._Symbol == "*" and (is_leaf(root._Left) or is_leaf(root._Right)):		
-		if debug:
-			print("Case: x*1 and x*0 ")
-			print(root)
-			print("\t"+str(root._Left))
-			print("\t"+str(root._Right))
-
-		if root._Left._LitVal == 0 or root._Right._LitVal == 0:			
-			zeroed( root )	
-				
-
-		elif root._Left._LitVal == 1:
-			if parent:
-				if direction == "left":
-					parent._Left = root._Right
-				else: 
-					parent._Right = root._Right
-			else:
-				root = root._Right
-			
-
-		elif root._Right._LitVal == 1:	
-			if parent:
-				if direction == "left":
-					parent._Left = root._Left
-				else: 
-					parent._Right = root._Left
-			else:
-				root = root._Left
-			
-
-			#set_child(parent, root, direction, root._Left)
-			
-			
-		
-	# --------------------- Simplifies n * m ----------------------------------#
-	if (root and root._Symbol == "*" and 
-								(is_leaf(root._Left) and is_leaf(root._Right))):
-		#print(root)
-		#print(root._Left)
-		#print(root._Right)
-		if check(root._Left, "Literal") and check(root._Right, "Literal"):
-			
-			int_val = root._Left._LitVal * root._Right._LitVal
-			
-			int_node = Node( NodeType.Literal, str(int_val), int_val, None, None) 
-			
-			set_child(parent, root, direction, int_node)
-
-			
-
-			
-		
-	# --------------------- Simplifies n + m ----------------------------------#
-	if (root and root._Symbol == "+" and 
-								(is_leaf(root._Left) and is_leaf(root._Right))):
-		
-		if check(root._Left, "Literal") and check(root._Right, "Literal"):			
-			int_val = root._Left._LitVal + root._Right._LitVal
-			int_node = Node( NodeType.Literal, str(int_val), int_val, None, None) 
-			set_child(parent, root, direction, int_node)
-			
-			if root._Symbol == "+": 
-				root._Class = NodeType.Literal
-				root._Symbol = str(int_val)
-				root._LitVal = int_val
-				root._Left = None
-				root._Right = None
-			
-
-	# --------------------- Simplifies x + 0 	----------------------------#
-	# If we have a addition of zero and ONLY one of the children are leaf nodes:
-	if root and root._Symbol == "+" and (is_leaf(root._Left) or is_leaf(root._Right)):		
-		if root._Left._LitVal == 0:
-			#set_child(parent, root, direction, root._Right)
-			if parent:
-				if direction == "left":
-					parent._Left = root._Right
-				else: 
-					parent._Right = root._Right
-			else:
-				root = root._Right		
-					
-
-		elif root._Right._LitVal == 0:				
-			#set_child(parent, root, direction, root._Left)			
-			if parent:	
-				if direction == "left":
-					parent._Left = root._Left
-				else: 
-					parent._Right = root._Left
-			else:
-				root = root._Left
-			
-				
+	print(Fore.GREEN+"Input Information:"+Style.RESET_ALL)
+	print(80*"-")
+	print(Fore.GREEN+"Input String:"+Style.RESET_ALL,input_string)
 	
-	# --------------------- Simplifies 0 - x ----------------------------#
-	if (root and root._Symbol == "-" and is_leaf(root._Left) 
-									   and root._Left._Symbol == "0"):
-		pass
-
-	# --------------------- Simplifies x - 0 ----------------------------#
-	if (root and root._Symbol == "-" and is_leaf(root._Right) 
-									 and root._Right._Symbol == "0"):
-		set_child(parent, root, direction, root._Right)
-		
-
-		# if parent:
-		# 	if direction == "left":
-		# 		parent._Left = root._Left
-		# 	else: 
-		# 		parent._Right = root._Left
-		# else:			
-		# 	root = root._Left
+	# Lex the input string into tokens:
+	#-------------------------------------------------------------------------#
+	from lexer import Lexer 
+	foo = Lexer()
+	token_stream = foo.Lex(input_string)	
+	#-------------------------------------------------------------------------#
 	
-	# if root:
-	# 	#if tree_modified: simplify(root._Left, direction, root, debug)
-	# 	if not is_leaf(root._Left): simplify(root._Left,  "left", root, debug)	
-	# 	if not is_leaf(root._Right):simplify(root._Right, "right",root, debug)
+	print(Fore.GREEN+"\nToken Stream:"+Style.RESET_ALL)
+	print(token_stream)
 
-	return root
+	initilize(token_stream)
+	#print("Input String Length:", len(args[1]))	
 	
-def set_child(parent, root, direction, new_child):
-	if parent:
-		if direction == "left":
-			parent._Left = new_child
-		else: 
-			parent._Right = new_child
-	else:
-		root = new_child
+	# Build The Expression Tree: 
+	#-------------------------------------------------------------------------#
+	root = Expression(debug=False)
+	#-------------------------------------------------------------------------#
 
-def simplify_mult( root ):
-	""" 
-		Combine coefficients in the expression tree pointed to by root	
-		This is a recursive method which looks through the tree for linked
-		multiplication nodes.
-
-		If it finds linked multiplication nodes, it will call reduce_coefficents()
-	"""
-
-	if root._Symbol == "*" and (root._Left._Symbol == "*" or root._Right._Symbol == "*"):
-		root = reduce_coefficents( root )
-
-	else:
-		if root._Left: simplify_mult(root._Left)
-		if root._Right: simplify_mult(root._Right)
-
-	return root
-
-def reduce_coefficents( root ):	
-	""" 
-		Finds all coefficients in a given set of linked multiplication nodes
-		and combines them into a single integer.
-	"""
-	nodes = []
-	vals = []
-	gather_coefficients( root, nodes )
+	#print(Fore.GREEN+"\nNumber of nodes:"+Style.RESET_ALL, count_nodes( root ))
+	reset_seen( root )
 	
-	# Change every coefficient to '1':
-	for node in nodes: 
-		vals.append(node._LitVal)
-		node._LitVal = 1
-		node._Symbol ="1"
+	print(Fore.GREEN+"\nNormalized input string:"+Style.RESET_ALL	)
+	PrintNormalizedExpression( root ) 
+	print("\n")
+
+	print(80*"-")
+	print(Fore.GREEN+"Input Expression Tree:"+Style.RESET_ALL)
+	PrintTree( root )
+	print()
 	
-	coef = 1
-
-	# Compute the new, singular coefficient:
-	for val in vals: coef *= val
-
-	# Apply that coefficient to to the leftmost node in our tree:	
-	nodes[-1]._Symbol = str(coef)
-	nodes[-1]._LitVal = coef
-
-	# Because we changed every coefficient to '1', we call simplify again
-	# to delete those nodes. 
-	return simplify( root,  direction=None, parent=None, debug=False)
-
-def gather_coefficients( root, coefs):
-	"""
-		Grabs all coefficients in a set of linked multiplication nodes.
-		The leftmost node will be in the LAST index of the list.
-	"""
-	if root._Symbol == "*": 			
-		left =  is_leaf(root._Left)
-		right = is_leaf(root._Right)
-		
-		if left and str(root._Left._Class) == str(NodeType.Literal):
-			#print("Added a left coef")
-			coefs.append(root._Left)
-
-		elif right and str(root._Right._Class) == str(NodeType.Literal):
-			#print("Added a right coef")
-			coefs.append(root._Right)
+	#print(80*"=")
+	#print("Tree Dump:\n")
+	#DumpTree(root, 0)
+	#print(80*"-")
 	
-		if not left and root._Left._Symbol == "*":
-			gather_coefficients( root._Left, coefs)		
-		
-		if not right and root._Right._Symbol == "*":
-			gather_coefficients( root._Right, coefs)
+	#return 0
 	
-	return coefs
+	#======================== Derivative Calculating =========================#
+	
+	from calculator import diff, simplify, simplify_mult
+	
+	# Differentiate the expression:
+	#-------------------------------------------------------------------------#
+	result = diff( root )
+	#-------------------------------------------------------------------------#
 
-def xor( a , b ):
-	if a and not b: return True
-	if b and not a: return True
-	if b and a: 	return False
-	if b or a: 		return False
+	print(80*"-")
+	print(Fore.GREEN+"Derivative Result Tree:"+Style.RESET_ALL)
+	PrintTree( result )
 
-def zeroed( root ):	
-	""" Zeroes out a node, and set's it's children to None."""
-	root._Class = NodeType.Literal
-	root._Symbol = "0"
-	root._LitVal =  0	
-	root._Left = None
-	root._Right = None
-		
+	#print("\nResult Tree Dump:")
+	#DumpTree(result,0)
+	
+	print(80*"-")	
+	print(Fore.GREEN+"Derivative Result Expression:"+Style.RESET_ALL)
+	PrintNormalizedExpression( result )
+	print("\n")
+	
+	# Simplify the result expression tree:
+	#-------------------------------------------------------------------------#
+	simp = simplify( result )
+	#-------------------------------------------------------------------------#
 
-def is_leaf(node):
-	if node and node._Left == None and node._Right == None:
-		return True
-	else:
-		return False
+	# Reduce coeffecients:
+	#-------------------------------------------------------------------------#
+	simp = simplify_mult( simp )
+	simp = simplify( simp )
+	#-------------------------------------------------------------------------#
 
+	#simp = simplify( simp 	)			
+	#simp = simplify( simp 	)			
+	#simp = simplify( simp, direction=None, parent=None, debug=True	)			
 
-def intg(expression_tree):
-	return None
-
-
+	print(80*"-")		
+	print(Fore.GREEN+"Simplified Derivative Result Tree:"+Style.RESET_ALL)
+	PrintTree( simp )	
+	print()
+	
+	print(80*"-")
+	print(Fore.GREEN+"Simplified Derivative Result Expression:"+Style.RESET_ALL)
+	PrintNormalizedExpression( simp )
+	print("\n")
 
 if __name__ == "__main__":
-	print("Main func tests:\n===========================")
+	
